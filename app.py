4c960f1fb79b66bcb719744f278bb4da00a2bee9
YoussefUQAC <ychakrou@uqac.ca>
2025-07-19 19:26:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -164,9 +164,9 @@ def update_resultats(selected_codes_groups):
             data=df_resume.to_dict('records'),
             columns=[{'name': col, 'id': col} for col in df_resume.columns],
             style_table={'overflowX': 'auto'},
-            style_cell={'textAlign': 'center'}
+            style_cell={'textAlign': 'ceynter'}
         )
     ])
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8050)
+    app.run_server(debug=True, host='0.0.0.0', port=8050)
