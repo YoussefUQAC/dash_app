ea2c2f3cc6c357e99eef363f1f3ea457ac1dd0e7
YoussefUQAC <ychakrou@uqac.ca>
2025-07-19 23:52:26 -0400

    Delete assets directory

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('🎉 Dash page loaded successfully');
-    // Exemple : ajouter une alerte après chargement
-    setTimeout(() => {
-        alert("Bienvenue sur votre app Dash !");
-    }, 2000);
-});
-
